d5d0a956dbbbdd58898f96a59e2d7dffdaff896a
CaiJi <jvfsdfkfwbdsbwsdj@foxmail.com>
2020-10-18 20:18:32 +0800

    ....

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .gacha import gacha_10
 sv = Service('原神抽卡')
 
 
-@sv.on_prefix(["相遇之源"], only_to_me=True)
+@sv.on_prefix(["相遇之缘"], only_to_me=True)
 async def gacha_(bot, ev):
     await bot.send(ev, gacha_10())
 
