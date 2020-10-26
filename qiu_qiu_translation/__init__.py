dbccfd5bca46df8bceda37ec2e855012d7eda97c
CaiJi <jvfsdfkfwbdsbwsdj@foxmail.com>
2020-10-26 16:29:10 +0800

    添加丘丘语翻译插件

---
+++ qiu_qiu_translation/__init__.py
@@ -0,0 +1,32 @@
+from hoshino import Service
+from .qiu_qiu_translation import qiu_qiu_word_translation,qiu_qiu_phrase_translation
+
+
+sv = Service("丘丘语翻译")
+
+suffix = "\n※ 发送词语时请注意空格位置是否正确，词语不区分大小写，不要加入任何标点符号\n※ 翻译数据来源于 米游社论坛 https://bbs.mihoyo.com/ys/article/2286805 \n※ 如果你有更好的翻译欢迎来提出 issues"
+
+
+
+@sv.on_prefix("丘丘一下")
+async def qiu_qiu(bot, ev):
+    txt = ev.message.extract_plain_text().strip().lower()
+    mes = qiu_qiu_word_translation(txt)
+    mes += suffix
+    await bot.send(ev, mes)
+
+
+
+@sv.on_prefix("丘丘词典")
+async def qiu_qiu(bot, ev):
+    txt = ev.message.extract_plain_text().strip().lower()
+    mes = qiu_qiu_phrase_translation(txt)
+    mes += suffix
+    await bot.send(ev, mes)
+
+
+
+
+
+
+
