764781f0c924e2e9839d4d1d72ef208ba2ad1b55
CaiJi <jvfsdfkfwbdsbwsdj@foxmail.com>
2020-10-31 20:52:11 +0800

    加入了查找资源地图的功能，修bug

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from hoshino import Service
 sv = Service("原神帮助")
 
 
-help_txt = '''这是一个HoshinoBot的原神相关插件，包含原神抽卡，丘丘语翻译，找神瞳等功能
+help_txt = '''这是一个HoshinoBot的原神相关插件，包含原神抽卡，丘丘语翻译，找神瞳,找资源点等功能
 插件仓库在 https://github.com/H-K-Y/Genshin_Impact_bot.git
 
 指令：
