d608458cb9cb799bcb24bb986e054df7b3e8ec19
wansn <2632324507@qq.com>
2021-06-05 22:26:32 +0800

    格式化
    按照PEP标准进行格式化

--- almanac/almanac.py
+++ almanac/almanac.py
@@ -1,4 +1,4 @@
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
 import os
@@ -7,27 +7,28 @@ import random
 import base64
 import time
 
-
 FILE_PATH = os.path.dirname(__file__)
-FONT_PATH = os.path.join(FILE_PATH,"汉仪文黑.ttf")
+FONT_PATH = os.path.join(FILE_PATH, "assets", "汉仪文黑.ttf")
+LIST_PATH = os.path.join(FILE_PATH, "assets", "almanac_list.json")
+BG_PATH = os.path.join(FILE_PATH, "assets", "back.png")
 
-data = {} # config.json里的数据
+data = {}  # config.json里的数据
 
 almanac_data = {
     # 生成的黄历base64字符串和黄历更新日期
-    "date" : "",
-    "almanac_base64_str" :""
+    "date": "",
+    "almanac_base64_str": ""
 }
 
+chinese = {"0": "", "1": "一", "2": "二", "3": "三", "4": "四", "5": "五", "6": "六", "7": "七", "8": "八", "9": "九"}
 
-chinese = {"0":"","1":"一","2":"二","3":"三","4":"四","5":"五","6":"六","7":"七","8":"八","9":"九"}
 
-def month_to_chinese(month:str):
+def month_to_chinese(month: str):
     # 把日期数字转成中文数字
     m = int(month)
-    if m < 10 :
+    if m < 10:
         return chinese[month[-1]]
-    elif m < 20 :
+    elif m < 20:
         return "十" + chinese[month[-1]]
     else:
         return chinese[month[0]] + "十" + chinese[month[-1]]
@@ -36,21 +37,22 @@ def month_to_chinese(month:str):
 def load_data():
     # 载入config.json文件的数据
     global data
-    with open(os.path.join(FILE_PATH, 'config.json'), 'r', encoding='UTF-8') as f:
+    with open(os.path.join(FILE_PATH, LIST_PATH), 'r', encoding='UTF-8') as f:
         data = json.load(f)
 
     almanac_data["date"] = ""
     almanac_data["almanac_base64_str"] = ""
 
+
 load_data()
 
-def seed_random_list(seed:str,l:list):
+
+def seed_random_list(seed: str, l: list):
     # 使用随机种子随机选择列表中的元素，相同的种子和列表将返回同样的输出
     seed = seed + str(l)
     random.seed(seed)
     index = random.random() * len(l)
     return l[int(index)]
-
 
 
 def generate_almanac():
@@ -63,22 +65,25 @@ def generate_almanac():
 
     while len(today_luck) < 6:
         # 随机6个不同的运势放到 today_luck
-        r = seed_random_list(str(offset) + seed , l)
+        r = seed_random_list(str(offset) + seed, l)
         if r in today_luck:
             offset += 1
         else:
             today_luck.append(r)
 
-    back = Image.open(os.path.join(FILE_PATH, "back.png"))
+    back = Image.open(BG_PATH)
 
     year = time.strftime("%Y")
     month = month_to_chinese(time.strftime("%m")) + "月"
-    day = month_to_chinese(time.strftime("%d")) + "日"
+    day = month_to_chinese(time.strftime("%d"))
 
     draw = ImageDraw.Draw(back)
-    draw.text((118, 165), year, fill="#8d7650ff", font=ImageFont.truetype(FONT_PATH, size=30), anchor="mm",align="center")
-    draw.text((260, 165), day, fill="#f7f8f2ff", font=ImageFont.truetype(FONT_PATH, size=35), anchor="mm",align="center")
-    draw.text((410, 165), month, fill="#8d7650ff", font=ImageFont.truetype(FONT_PATH, size=30), anchor="mm",align="center")
+    draw.text((118, 165), year, fill="#8d7650ff", font=ImageFont.truetype(FONT_PATH, size=30), anchor="mm",
+              align="center")
+    draw.text((260, 165), day, fill="#f7f8f2ff", font=ImageFont.truetype(FONT_PATH, size=35), anchor="mm",
+              align="center")
+    draw.text((410, 165), month, fill="#8d7650ff", font=ImageFont.truetype(FONT_PATH, size=30), anchor="mm",
+              align="center")
 
     buff = Image.new("RGBA", (325, 160))
     debuff = Image.new("RGBA", (325, 160))
@@ -88,10 +93,10 @@ def generate_almanac():
 
     for i in range(3):
         buff_name = today_luck[i]
-        debuff_name = today_luck[(i+3)]
+        debuff_name = today_luck[(i + 3)]
 
-        buff_effect = seed_random_list(seed , data[buff_name]["buff"] )
-        debuff_effect = seed_random_list(seed , data[debuff_name]["debuff"] )
+        buff_effect = seed_random_list(seed, data[buff_name]["buff"])
+        debuff_effect = seed_random_list(seed, data[debuff_name]["debuff"])
 
         buff_draw.text((0, i * 53), buff_name, fill="#756141ff", font=ImageFont.truetype(FONT_PATH, size=25))
         debuff_draw.text((0, i * 53), debuff_name, fill="#756141ff", font=ImageFont.truetype(FONT_PATH, size=25))
@@ -111,7 +116,6 @@ def generate_almanac():
 
 
 def get_almanac_base64_str():
-
     # if almanac_data["date"] == time.strftime("%Y-%m-%d"):
     #     return almanac_data["almanac_base64_str"]
     # else:
@@ -119,4 +123,3 @@ def get_almanac_base64_str():
     #     return almanac_data["almanac_base64_str"]
     generate_almanac()
     return almanac_data["almanac_base64_str"]
-
