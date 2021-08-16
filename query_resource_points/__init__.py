8103311e5f0e33a7a6417215ee53bf7ba90e6503
H-K-Y <33424676+H-K-Y@users.noreply.github.com>
2021-08-16 22:27:21 +0800

    修复bug
    修复bug

--- query_resource_points/__init__.py
+++ query_resource_points/__init__.py
@@ -1,31 +1,22 @@
 
-from nonebot import on_command,on_startswith,on_endswith
+from nonebot import on_command,on_regex
 from nonebot.adapters.cqhttp import Message
 from nonebot.adapters import Bot, Event
 from .query_resource_points import get_resource_map_mes,get_resource_list_mes,up_label_and_point_list,up_map
 
-inquire_resource_startswith = on_startswith(('哪有', '哪里有'))
-inquire_resource_endswith = on_endswith(('在哪', '在哪里', '哪有', '哪里有'))
+inquire_resource = on_regex(r"^哪里有|^哪有|在哪里$|在哪$")
 resource_list = on_command('原神资源列表')
 up_resource_list = on_command('刷新原神资源列表')
 up_map_icon = on_command('更新原神地图')
 
 
-
-async def _inquire_resource_(bot: Bot, event: Event):
+@inquire_resource.handle()
+async def _inquire_resource_(bot: Bot, event: Event,state:dict):
     resource_name = str(event.get_message()).strip()
+    resource_name = resource_name.replace(state["_matched"],"")
     if resource_name == "":
         return
-    await bot.send(Message(get_resource_map_mes(resource_name)), at_sender=True)
-
-@inquire_resource_startswith.handle()
-async def inquire_resource_startswith_(bot: Bot, event: Event):
-    await _inquire_resource_(bot,event)
-
-
-@inquire_resource_endswith.handle()
-async def inquire_resource_endswith_(bot: Bot, event: Event):
-    await _inquire_resource_(bot,event)
+    await inquire_resource.finish(Message(get_resource_map_mes(resource_name)), at_sender=True)
 
 
 
