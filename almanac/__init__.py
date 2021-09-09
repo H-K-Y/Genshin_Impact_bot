f305db88a503178abdf9deb61817926df9269ff3
Minori <memorydewey@vip.qq.com>
2021-09-10 02:11:34 +0800

    feat: 简写黄历抽签

--- almanac/__init__.py
+++ almanac/__init__.py
@@ -28,7 +28,7 @@ with open(os.path.join(FILE_PATH, 'group_list.json'), 'r', encoding='UTF-8') as 
     group_list = json.load(f)
 
 
-@sv.on_fullmatch('原神黄历')
+@sv.on_fullmatch('黄历')
 async def send_almanac(bot, ev):
     almanac_base64 = get_almanac_base64_str()
     mes = f"[CQ:image,file={almanac_base64}] \n ※ 黄历数据来源于 genshin.pub"
@@ -79,7 +79,7 @@ async def almanac_remind():
         await bot.send_group_msg(group_id=int(gid), message=mes)
 
 
-@sv.on_fullmatch('原神抽签')
+@sv.on_fullmatch('抽签')
 async def draw_lots(bot, ev):
     uid = str(ev['user_id'])
     quser = jdb.user(uid)
@@ -88,7 +88,7 @@ async def draw_lots(bot, ev):
         result = draw_info(quser.pos)
         draw_pic = gen_pic(result)["pic"]
         cq_str = get_cq(draw_pic)
-        msg = f"{cq_str}\n今天已经抽过签啦，明天再来吧~\n ※ 抽签条目来源于 genshin.pub"
+        msg = f"{cq_str}\n今天已经抽过签啦，明天再来吧~\n"
     else:
         draw_result = get_pic()
         pos = draw_result["pos"]
@@ -97,7 +97,7 @@ async def draw_lots(bot, ev):
 
         jdb.user(uid).write(pos)
         jdb.save()
-        msg = f"{cq_str}\n ※ 抽签条目来源于 genshin.pub"
+        msg = f"{cq_str}\n"
 
     await bot.send(ev, msg, at_sender=True)
 
@@ -109,7 +109,7 @@ async def answer_lots(bot, ev):
 
     try:
         answer = draw_info(quser.pos)["answer"]
-        msg = f'解签：{answer}\n ※ 解签条目来源于 genshin.pub'
+        msg = f'解签：{answer}\n'
     except KeyError:
         msg = '你还没抽过签哦~向我说“原神抽签”试试吧~'
     await bot.send(ev, msg, at_sender=True)
