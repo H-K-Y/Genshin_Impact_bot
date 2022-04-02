03604732b8e0f8bc21c0ab6634e5493b648ff74f
chingkingm <chingkingm@gmail.com>
2022-04-02 09:23:54 +0800

    🐛 Fix CQ Injection

--- query_resource_points/__init__.py
+++ query_resource_points/__init__.py
@@ -1,23 +1,23 @@
 
+from loguru import logger
 from hoshino import Service
-from .query_resource_points import get_resource_map_mes,get_resource_list_mes,init_point_list_and_map
+from .query_resource_points import get_resource_map_mes, get_resource_list_mes, init_point_list_and_map
 
 sv = Service("原神资源查询")
 
-@sv.on_suffix(('在哪', '在哪里', '哪有', '哪里有'))
-@sv.on_prefix(('哪有', '哪里有'))
-async def inquire_resource_points(bot, ev):
 
-    resource_name = ev.message.extract_plain_text().strip()
+@sv.on_rex(r"(\w+)(?:在哪|在哪里|哪有|哪里有)")
+@sv.on_rex(r"(?:哪有|哪里有)(\w+)")
+async def inquire_resource_points(bot, ev):
+    resource_name = ev['match'].group(1)
     if resource_name == "":
         return
 
     await bot.send(ev, await get_resource_map_mes(resource_name), at_sender=True)
 
 
-
 @sv.on_fullmatch('原神资源列表')
-async def inquire_resource_list(bot , ev):
+async def inquire_resource_list(bot, ev):
     # 长条消息经常发送失败，所以只能这样了
     mes_list = []
     txt_list = get_resource_list_mes().split("\n")
@@ -27,23 +27,21 @@ async def inquire_resource_list(bot , ev):
             "data": {
                 "name": "色图机器人",
                 "uin": "2854196310",
-                "content":txt
-                    }
-                }
+                "content": txt
+            }
+        }
         mes_list.append(data)
     # await bot.send(ev, get_resource_list_mes(), at_sender=True)
     await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
 
 
 @sv.on_fullmatch('刷新原神资源列表')
-async def inquire_resource_list(bot , ev):
+async def refresh_resource_list(bot, ev):
     await init_point_list_and_map()
     await bot.send(ev, '刷新成功', at_sender=True)
 
 
-
 @sv.on_fullmatch('更新原神地图')
-async def up_map_icon(bot , ev):
+async def up_map_icon(bot, ev):
     await init_point_list_and_map()
     await bot.send(ev, '更新成功', at_sender=True)
-
