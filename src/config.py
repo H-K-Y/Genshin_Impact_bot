ab6fe567f5c8e284eddddcf96d3ede559dd41b3b
Wansn <2632324507@qq.com>
2023-03-02 11:48:59 +0800

    💩 另一种可能的config实现
    !氵代码 懒得想json怎么搞注释了

--- src/config.py
+++ src/config.py
@@ -1,81 +1,91 @@
+import json
+from abc import ABC, abstractmethod
 from pathlib import Path
 from collections import UserDict
-from typing import Callable, Optional, Union, Type, TypeVar, Dict
+from typing import Callable, Optional, Union, Type, TypeVar, Dict, Literal
 
+import tomlkit
+from pydantic import BaseModel, create_model
 from tomlkit import TOMLDocument
 from tomlkit.items import Table, Trivia
 from .utils import logger
 
 
-TomlAvailType = Union[str, bool, int, float, list, tuple, dict]
-TomlAvailTypes = (str, bool, int, float, list, tuple, dict)
-TomlAvailClasses = TypeVar('TomlAvailClasses', bound=TomlAvailType)
+class SimpleLoaderBase(ABC):
+    def __init__(self, path: Union[str, Path], encoding: str = 'utf-8'):
+        if not isinstance(path, Path):
+            path = Path(Path)
+        self.__path = path
+        self.encoding = encoding
+
+    @abstractmethod
+    def load(self):
+        ...
+
+    @abstractmethod
+    def save(self, __obj):
+        ...
+
+    @abstractmethod
+    async def async_load(self):
+        ...
+
+    @abstractmethod
+    async def async_save(self, __obj):
+        ...
 
 
-class ConfigField:
-    description: str
-    """
-    Fields of config, note that `tuple` will be automatically converted to list(array)
-    """
-    def __init__(self, __type: Type[TomlAvailClasses],
-                 description: Optional[Union[str, None]] = None,
-                 default: Optional[Union[TomlAvailType, None]] = None):
-        if __type not in {str, bool, int, float, list, tuple, dict}:
-            self._type = str
-        else:
-            self._type = __type
-        self.description = description
-        if isinstance(default, self._type):
-            self._value = default
-        else:
-            self._value = self._type()
-
-    def update(self, value: TomlAvailType):
-        if isinstance(value, self._type):
-            self._value = value
-        else:
-            self._value = self._type(value)
-
-    def clear(self):
-        self._value = self._type()
-
-    @property
-    def value(self):
-        return self._value
-
-    @property
-    def type(self):
-        return self._type
+class JSONLoader(SimpleLoaderBase):
+    def __dumps_data(self, __data: dict, __schema: dict):
+        _result = {}
+        _properties = __schema['properties']
+        for k, v in _properties:
+            ...
 
 
-class BaseContainer(UserDict):
-    def __init__(self, on_error: Optional[Callable] = logger.error):
-        self.__on_error = on_error
-        self._update_fields()
-        super().__init__()
+    def load(self):
+        with open(self.__path, 'r', encoding=self.encoding) as _config_file:
+            return json.load(_config_file)
 
-    def _update_fields(self):
-        _fields = filter(lambda x: not x[0].startswith('_'), self.__class__.__dict__.items())
-        for _field_name, _field_definition in _fields:
-            if _field_definition in {str, bool, int, float, list, tuple}:
-                self[_field_name] = ConfigField(_field_definition)
-            if isinstance(_field_definition, ConfigField):
-                self[_field_name] = _field_definition
+    def save(self, __obj: 'ConfigBase'):
+        _properties = __obj.schema().get('properties')
+        _data = __obj.dict()
+        with open(self.__path, 'w+', encoding=self.encoding) as _config_file:
+            json.dump(obj=__obj, fp=_config_file, ensure_ascii=False)
 
+    async def async_load(self):
+        return self.load()
 
-class ConfigTable(BaseContainer):
-    def __init__(self, **fields: Union[Type[ConfigField], Type[TomlAvailType]]):
-        super().__init__()
-        self._update_fields()
-
-        for _field_name, _field_definition in fields.items():
-            if _field_definition in {str, bool, int, float, list, tuple}:
-                self[_field_name] = ConfigField(_field_definition)
-            if isinstance(_field_definition, ConfigField):
-                self[_field_name] = _field_definition
+    async def async_save(self, __obj):
+        return self.save(__obj)
 
 
-class ConfigBase(BaseContainer):
+class TomlLoader(SimpleLoaderBase):
+    def load(self):
+        with open(self.__path, 'r', encoding=self.encoding) as _config_file:
+            return tomlkit.load(_config_file)
+
+    def save(self, __obj):
+        with open(self.__path, 'w+', encoding=self.encoding) as _config_file:
+            tomlkit.dump(__obj, _config_file)
+
+    async def async_load(self):
+        return self.load()
+
+    async def async_save(self, __obj):
+        return self.save(__obj)
+
+
+def get_loader(file_ext: str):
+    file_ext = file_ext.lower()
+    if file_ext == 'json':
+        return JSONLoader
+    if file_ext == 'toml':
+        return TomlLoader
+    return None
+
+
+class ConfigBase(BaseModel):
     """
     ConfigBase.
 
@@ -86,4 +96,28 @@ class ConfigBase(BaseContainer):
         config = MyConfig('./config.toml')
         assert config.foo == "bar"
     """
-    ...
+
+    @staticmethod
+    def __check_config_ext(path: Union[str, Path]):
+        if not isinstance(path, Path):
+            path = Path(path)
+        _name_split = path.name.lower().split()
+        if len(_name_split) <= 2:
+            return None
+        if _name_split[-1] in ['json', 'toml', 'conf', 'ini']:
+            return _name_split[-1]
+        return None
+
+    def reload(self):
+        self.__data = self.__loader.load()
+        self.model = self.parse_obj(self.__data)
+
+    def save(self):
+        ...
+
+    def __init__(self, path: Union[str, Path]):
+        _loader = get_loader(self.__check_config_ext(path))
+        self.__config_path = path
+        self.__loader: SimpleLoaderBase = _loader(path)
+        self.__data = self.__loader.load()
+        self.model = super().__init__(**self.__data)
